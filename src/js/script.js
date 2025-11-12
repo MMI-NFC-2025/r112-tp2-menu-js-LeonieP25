1e9713b372c925a7ba16cc41777a77f7c5d32841
LeonieP25 <leonie.pruniaux@edu.univ-fcomte.fr>
2025-11-12 13:51:59 +0100

    Retrait des // verts

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,15 @@
-// On cible les éléments à modifier
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
-const body = document.body; // ajout pour cibler le body
+const body = document.body; 
 
-// Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
 toggle.addEventListener("click", () => {
   const isOpen = toggle.ariaExpanded === "true";
   const isClosed = !isOpen;
   console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
 
-  // Mise à jour des attributs ARIA pour accessibilité
   nav.ariaHidden = isOpen;
   toggle.ariaExpanded = isClosed;
 
-  // Utilisation de classList.toggle()
   body.classList.toggle("noscroll", isClosed);
 });
 
